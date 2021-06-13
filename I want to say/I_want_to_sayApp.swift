098677d621b1d9290a738968938f24dda0f28261
Evgeny Kolesnik <grid.box@gmail.com>
2021-06-13 17:10:03 +0300

    добавление проекта

---
+++ I want to say/I_want_to_sayApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_want_to_sayApp.swift
+//  I want to say
+//
+//  Created by Evgeny Kolesnik on 13.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct I_want_to_sayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
